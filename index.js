50739c7143be7518ad6723935c5d8f48792666a6
StarLight229 <45467610+StarLight229@users.noreply.github.com>
2018-12-01 16:17:46 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const client = new Discord.Client();
+
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+var prefix = ("sigbot!")
+
+
+bot.on('ready', function() {
+    bot.user.setUsername("NAME")
+    bot.user.setPresence({ game: { name: 'GAME'}, status: 'online'})
+    console.log("Connected")});
+
+bot.login(process.env.TOKEN);
+
+
+
+bot.on('message', message => {
+    if(message.content.startsWith(prefix + "DM")) {
+
+        if(message.mentions.users.first()){
+
+        let messageToSend = message.content.split(" ").slice(2).join(" ");
+        let userToSend = message.mentions.users.first();
+
+        userToSend.send(`${message.author.username} vous a envoyer un message!\n${messageToSend}`);
+        message.delete(`${message.author.username} vous a envoyer un message!\n${messageToSend}`);
+        message.channel.send(`**${message.author}** , votre message a bien été envoyer a **${message.mentions.users.first().username}** :D`)
+}else{
+    message.delete();
+    message.channel.send(`erreur`)
+
+}
+}
+}
+)
